4dcc5752bc78a2eb2bd43c9624e397d05e1ad246
KeianBarton <Keian.Barton@civica.co.uk>
2018-06-06 16:44:08 +0100

    Added enter animation

--- WhatsThatFoodClient/src/app/app.component.ts
+++ WhatsThatFoodClient/src/app/app.component.ts
@@ -1,9 +1,46 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('bounceInFromTop', [
+      state('in', style({transform: 'translateY(0)'})),
+      transition(':enter', [
+        animate('500ms ease-out', keyframes([
+          style({opacity: 0, transform: 'translateY(-100%)', offset: 0}),
+          style({opacity: 1, transform: 'translateY(+5%)', offset: 0.8}),
+          style({transform: 'translateY(0)', offset: 1})
+        ]))
+      ]),
+      transition(':leave', [
+        animate('500ms ease-in', keyframes([
+          style({transform: 'translateY(0)', offset: 0}),
+          style({opacity: 1, transform: 'translateY(+5%)', offset: 0.8}),
+          style({opacity: 0, transform: 'translateY(-100%)', offset: 1})
+        ]))
+      ])
+    ]),
+    trigger('bounceInFromBottom', [
+      state('in', style({transform: 'translateY(0)'})),
+      transition(':enter', [
+        animate('500ms ease-out', keyframes([
+          style({opacity: 0, transform: 'translateY(100%)', offset: 0}),
+          style({opacity: 1, transform: 'translateY(+5%)', offset: 0.8}),
+          style({transform: 'translateY(0)', offset: 1})
+        ]))
+      ]),
+      transition(':leave', [
+        animate('500ms ease-in', keyframes([
+          style({transform: 'translateY(0)', offset: 0}),
+          style({opacity: 1, transform: 'translateY(-5%)', offset: 0.8}),
+          style({opacity: 0, transform: 'translateY(100%)', offset: 1})
+        ]))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
 }
--- WhatsThatFoodClient/src/app/app.module.ts
+++ WhatsThatFoodClient/src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
@@ -19,7 +20,8 @@ import { LoadingComponent } from './shared/components/loading/loading.component'
     LoadingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
