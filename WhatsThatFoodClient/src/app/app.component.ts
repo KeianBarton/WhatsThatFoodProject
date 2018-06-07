f1744240bd44e5a8a98be0a097a6a2de44f17b01
KeianBarton <Keian.Barton@civica.co.uk>
2018-06-07 12:26:20 +0100

    Responsive styling

--- WhatsThatFoodClient/src/app/app.component.ts
+++ WhatsThatFoodClient/src/app/app.component.ts
@@ -6,6 +6,21 @@ import { trigger, state, style, animate, transition, keyframes } from '@angular/
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   animations: [
+    trigger('fadeIn', [
+      state('in', style({opacity: '1'})),
+      transition(':enter', [
+        animate('500ms ease-out', keyframes([
+          style({opacity: 0, offset: 0}),
+          style({opacity: 1, offset: 1})
+        ]))
+      ]),
+      transition(':leave', [
+        animate('500ms ease-in', keyframes([
+          style({opacity: 1, offset: 0}),
+          style({opacity: 0, offset: 1})
+        ]))
+      ])
+    ]),
     trigger('bounceInFromTop', [
       state('in', style({transform: 'translateY(0)'})),
       transition(':enter', [
--- WhatsThatFoodClient/src/app/home/home.component.ts
+++ WhatsThatFoodClient/src/app/home/home.component.ts
@@ -12,4 +12,8 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
+  triggerFileInput() {
+    document.getElementById('upload-input').click();
+  }
+
 }
